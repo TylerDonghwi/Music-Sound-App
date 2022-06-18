b49514c7aed7f0df66094e946758d946c2a58469
TylerDonghwi <tylerdonghwikim@email.com>
2022-06-19 11:42:34 +1200

    ball implemented

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 window.addEventListener('load', () => {
     const sounds = document.querySelectorAll(".sound")
     const pads = document.querySelectorAll(".keyboard div")
+    const visual = document.querySelector(".visual")
+    const colors = [
+        "red",
+        "orangered",
+        "orange",
+        "yellow",
+        "greenyellow",
+        "yellowgreen ",
+        "green ",
+        "cornflowerblue ",
+        "blue ",
+        "navy ",
+        "purple"
+    ]
+
 
     // sound 
-    pads.forEach((pad, index) => {
+    pads.forEach((pad, i) => {
         pad.addEventListener('click', function() {
-            sounds[index].play();
+            sounds[i].currentTime = 0
+            sounds[i].play()
+            createEffect(i)
         })
     })
+
+    // Effects
+    const createEffect = (i) => {
+        const effect = document.createElement("div")
+        visual.appendChild(effect)
+        effect.style.backgroundColor = colors[i]
+        effect.style.animation = 'jump 2s ease'
+        effect.addEventListener('animationend', function() {
+            visual.removeChild(this);
+        })
+    }
 })
